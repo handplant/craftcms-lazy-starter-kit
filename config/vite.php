cee38d8360a21ec6206fb81fe01a38c9e7731976
Andi Grether <handplant@users.noreply.github.com>
2025-11-18 10:54:49 +0100

    fix(vite): adjust config

--- config/vite.php
+++ config/vite.php
@@ -5,9 +5,10 @@ use craft\helpers\App;
 $useDevServer = App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev';
 
 return [
-    'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
-    'devServerInternal' => 'https://localhost:3000',
+    'useDevServer' => $useDevServer,
+    'checkDevServer' => true,
+    'devServerInternal' => 'http://localhost:3000',
     'devServerPublic' => App::env('SITE_URL') . ':3000',
-    'serverPublic' => App::env('SITE_URL') . '/dist/',
-    'manifestPath' => '@webroot/dist/.vite/manifest.json',
+    'manifestPath' => Craft::getAlias('@webroot') . '/dist/.vite/manifest.json',
+    'serverPublic' => Craft::getAlias('@web') . '/dist/',
 ];
